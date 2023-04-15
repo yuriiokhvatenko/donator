c117f7d3f5de36c285ea1ee82a754f7ceca8bb0b
yuriiokhvatenko <113927739+yuriiokhvatenko@users.noreply.github.com>
2023-04-15 20:23:41 +0000

    Deploying to gh-pages from @ yuriiokhvatenko/donator@8e350f04512379cc8ae0ccdfe722231e4bb6aaaa 🚀

---
+++ index.655db9ff.js
@@ -0,0 +1,2 @@
+const t=document.querySelector(".header__btn");function e(){t.textContent="AGAINER"}console.log(t),t.addEventListener("click",(function(){t.textContent="CLICKED",t.addEventListener("click",e)}));
+//# sourceMappingURL=index.655db9ff.js.map
---
+++ index.8fb5b72d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(".header__btn");function e(){t.textContent="AGAINER"}console.log(t),t.addEventListener("click",(function(){t.textContent="CLICKED",t.addEventListener("click",e)}))}();
+//# sourceMappingURL=index.8fb5b72d.js.map
